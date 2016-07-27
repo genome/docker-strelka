24cd119b79f43c700124ca6b89ccfde653ee3175
Thomas Mooney <tmooney@genome.wustl.edu>
2016-07-27 09:02:59 -0500

    Add options for config file and CPU count.
    Also... quote lots of variables!

--- docker_helper.sh
+++ docker_helper.sh
@@ -3,18 +3,38 @@
 set -o errexit
 set -o nounset
 
-if [ $# -ne 4 ]
+if [ $# -ne 6 ]
 then
-    echo "Usage: $0 [TUMOR_BAM] [NORMAL_BAM] [REFERENCE] [OUTPUT_DIR]"
+    echo "Usage: $0 [TUMOR_BAM] [NORMAL_BAM] [REFERENCE] [OUTPUT_DIR] [CONFIG] [NUM_CPUS]"
     exit 255
 fi
 
-TUMOR_BAM=$1
-NORMAL_BAM=$2
-REFERENCE=$3
-OUTPUT_DIR=$4
+TUMOR_BAM="$1"
+NORMAL_BAM="$2"
+REFERENCE="$3"
+OUTPUT_DIR="$4"
+CONFIG="$5"
+NUM_CPUS="$6"
 
-$STRELKA_INSTALL_DIR/bin/configureStrelkaWorkflow.pl --tumor=$TUMOR_BAM --normal=$NORMAL_BAM --ref=$REFERENCE --output-dir=$OUTPUT_DIR --config=$STRELKA_INSTALL_DIR/etc/strelka_config_bwa_default.ini
+#CONFIG can name of one of the default configuration files
+#Otherwise it must be the full path to an alternative configuration
+DEFAULT_CONFIG="$STRELKA_INSTALL_DIR/etc/$CONFIG"
+if [ -e "$DEFAULT_CONFIG" ]
+then
+    CONFIG="$DEFAULT_CONFIG"
+fi
 
-cd $OUTPUT_DIR
-make -j 8
+if [ -z "$NUM_CPUS" ]
+then
+    NUM_CPUS=8
+fi
+
+$STRELKA_INSTALL_DIR/bin/configureStrelkaWorkflow.pl \
+    --tumor="$TUMOR_BAM" \
+    --normal="$NORMAL_BAM" \
+    --ref="$REFERENCE" \
+    --output-dir="$OUTPUT_DIR" \
+    --config="$CONFIG"
+
+cd "$OUTPUT_DIR"
+make -j "$NUM_CPUS"
